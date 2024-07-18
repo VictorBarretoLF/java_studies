1199145d8ab455aa6bbec36f7c2a83ca6de64724
Víctor Barreto <victor.barreto@behoh.com>
2024-07-18 10:10:04 -0300

    feat: criado teste para cenario01

---
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,43 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void cenario01 () {
+        //idade 4 anos e 4kg - ALTA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+
+        CadastroPetDto cadastroPetDto = new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        );
+
+        Pet pet = new Pet(cadastroPetDto, abrigo);
+
+        CalculadoraProbabilidadeAdocao calcularoda = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidadeAdocao = calcularoda.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidadeAdocao);
+    }
+
+}
