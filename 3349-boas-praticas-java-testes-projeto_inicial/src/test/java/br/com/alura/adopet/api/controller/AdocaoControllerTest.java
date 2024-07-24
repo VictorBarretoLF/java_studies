1d88e5704bf215b2958525acc49738413008afe5
Victor Barreto <victorbarretolins@gmail.com>
2024-07-23 22:23:54 -0300

    feat: adicionado mvc teste para controller de adocao

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -6,18 +6,34 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(MockitoExtension.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdocaoControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    public void deveDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
+    public void deveDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
         // Arrange
         String json = "{}";
 
         // ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
 
         // ASSERT
-        Assertions.assertEquals(400, ?);
+        Assertions.assertEquals(400, response.getStatus());
     }
 }
