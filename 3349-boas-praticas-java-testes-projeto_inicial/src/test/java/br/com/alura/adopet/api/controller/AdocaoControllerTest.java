1706e0d8bbb7e4fedc4dc442c82d71ac0ef387b4
Victor Barreto <victorbarretolins@gmail.com>
2024-07-29 21:28:49 -0300

    feat: adicionad testes de integracao

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -4,10 +4,7 @@ import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,12 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @AutoConfigureJsonTesters
 class AdocaoControllerTest {
 
@@ -50,12 +49,33 @@ class AdocaoControllerTest {
     }
 
     @Test
+    public void deveDevolverCodigo400ParaSolicitacaoDeAdocaoSemErros() throws Exception {
+        // Arrange
+        String json = """
+                {
+                    "idPet": 1,
+                    "idTutor": 1,
+                    "motivo": "motivo do caramba"
+                }
+                """;
+
+        // ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // ASSERT
+        Assertions.assertEquals(200, response.getStatus());
+    }
+
+    @Test
     void deveriaDevolverCodigo200ParaRequsisicaoDeSolicitarAdocaoComDadosValidos() throws Exception {
         //ARRANGE
         SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(1L, 1L, "Motivo qualquer");
 
         BDDMockito.doNothing().when(adocaoService).solicitar(BDDMockito.any(SolicitacaoAdocaoDto.class));
-
         //ACT
         var response = mvc.perform(
                 post("/adocoes")
@@ -67,4 +87,5 @@ class AdocaoControllerTest {
         Assertions.assertEquals(200, response.getStatus());
         Assertions.assertEquals("Adoção solicitada com sucesso!", response.getContentAsString());
     }
+
 }
