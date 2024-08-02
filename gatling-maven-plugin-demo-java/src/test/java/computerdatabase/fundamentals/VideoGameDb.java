bf84c92664f7bf1f4a51c1d3fa357c5376e5802f
Victor Barreto <victorbarretolins@gmail.com>
2024-08-02 14:01:49 -0300

    feat: adicionado scripts gatling

---
+++ gatling-maven-plugin-demo-java/src/test/java/computerdatabase/fundamentals/VideoGameDb.java
@@ -0,0 +1,38 @@
+package computerdatabase.fundamentals;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class VideoGameDb extends Simulation {
+
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("https://videogamedb.uk/api")
+            .acceptHeader("application/json");
+
+    // Pause Docs - https://docs.gatling.io/reference/script/core/scenario/#pause
+    private ScenarioBuilder scn = scenario("Video Game Db - Section 5 code")
+
+            .exec(http("Get all video games - 1st call")
+                    .get("/videogame"))
+            .pause(5)
+
+            .exec(http("Get specific game")
+                    .get("/videogame/1"))
+            .pause(1, 10)
+
+            .exec(http("get all video games - 2nd call")
+                    .get("/videogame"))
+            .pause(Duration.ofMillis(4000));
+
+    {
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
+    }
+
+}
