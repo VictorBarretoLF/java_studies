cc57b7452476eb9e75c776441c6e62892cd95bbe
Victor Barreto <victorbarretolins@gmail.com>
2024-10-14 17:00:01 -0300

    feat: melhorado arquivos e pastas

---
+++ apache-kafka/HELPME.md
@@ -0,0 +1,3 @@
+kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste --from-beginning --group=x
+
+kafka-consumer-groups --bootstrap-server=localhost:9092 --group=x --describe
--- michelle_brito_microservices/ead/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ead.authuser.repositories;
-
-import com.ead.authuser.models.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<UserModel, UUID> {
-}
